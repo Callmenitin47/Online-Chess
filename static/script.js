39492bd2a8d6f32aec859ca75c52fdfed6552038
Callmenitin47 <nitink121196@gmail.com>
2024-07-08 11:52:40 +0530

    Added AJAX functions for user authentication (signup and login)

--- static/script.js
+++ static/script.js
@@ -7,3 +7,41 @@ function showLoginForm() {
     document.getElementById('signup-form').style.display='none';
     document.getElementById('login-form').style.display='block';
 }
+
+function submitSignupForm(event) {
+    event.preventDefault();
+    var formData = $('#newuser-form').serialize();
+    $.ajax({
+        url: '/signup',
+        type: 'POST',
+        data: formData,
+        success: function(response)
+        {
+            alert(response.message);
+            document.getElementById('signup').reset();
+        },
+        error: function(xhr, status, error)
+        {
+            var response=JSON.parse(xhr.responseText);
+            alert(response.error);
+        }  
+    });
+}
+
+function submitLoginForm(event) {
+    event.preventDefault();
+    var formData = $('#signin-form').serialize();
+    $.ajax({
+        url: '/login',
+        type: 'POST',
+        data: formData,
+        success: function(response)
+        {
+            window.location.href='/profile';
+        },
+        error: function(xhr, status, error)
+        {
+            alert("Login Username/Password is incorrect");
+        }  
+    });
+}
