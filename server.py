bba06a3b84f6d5f41fd2ca872c85ad3337c568b9
Callmenitin47 <nitink121196@gmail.com>
2024-07-18 18:34:39 +0530

    Added validation for moves for each type of chess piece

--- server.py
+++ server.py
@@ -21,6 +21,21 @@ match_history=db["games"]
 player_ids=[]
 sessions={}
 ongoing_matches={}
+pieces={
+'p':'url(../static/files/pieces/black_pawn.png)',
+'P':'url(../static/files/pieces/white_pawn.png)',
+'r':'url(../static/files/pieces/black_rook.png)',
+'R':'url(../static/files/pieces/white_rook.png)',
+'b':'url(../static/files/pieces/black_bishop.png)',
+'B':'url(../static/files/pieces/white_bishop.png)',
+'n':'url(../static/files/pieces/black_knight.png)',
+'N':'url(../static/files/pieces/white_knight.png)',
+'q':'url(../static/files/pieces/black_queen.png)',
+'Q':'url(../static/files/pieces/white_queen.png)',
+'k':'url(../static/files/pieces/black_king.png)',
+'K':'url(../static/files/pieces/white_king.png)',
+
+}
 
 app = Flask(__name__)
 app.secret_key = "enufwbqbiuwefbwebfuwergbfyewrgbuewrgbuyrbbwueuwen"
@@ -28,16 +43,16 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 socketio = SocketIO(app, manage_session=True, cors_allowed_origins='*')
 cors = CORS(app, resources={r"/socket.io/*": {"origins": "http://localhost:5000"}})
-
+player1=['p','n','r','k','q','b']
+player2=['P','N','R','K','Q','B']
 
 class Match:
 	def __init__(self,id1,id2):
-		self.id1=id1
-		self.id2=id2
+		self.players=[id1,id2]
 		self.turn=random.randint(0,1);
 		self.board=[
             ["r", "n", "b", "q", "k", "b", "n", "r"],
-            ["p", "p", "p", "p", "p", "p", "p", "p"],
+            ["q", "q", "q", "p", "p", "p", "p", "p"],
             ["", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", ""],
@@ -45,6 +60,8 @@ class Match:
             ["P", "P", "P", "P", "P", "P", "P", "P"],
             ["R", "N", "B", "Q", "K", "B", "N", "R"]
         ]
+		self.status="ongoing"
+		self.winner=-1
 
 
 def checkLogin():
@@ -363,21 +380,298 @@ def find_match(user_id):
 		socketio.emit('match_found', player2, room=user_id)
 		socketio.emit('match_found', player1, room=opponent_id)
 
- 
-
-
 @socketio.on('join_room')
 def join(user_session): 
 	if sessions[user_session['sid']] !=	int(user_session['id']):
 		return False
 	join_room(int(user_session['id']))
-	session['id']=user_session['id']
+	session['id']=int(user_session['id'])
 	find_match(user_session['id'])
 
 @socketio.on('join_match')
 def join_match(match_details):
 	session['room_id']=match_details['room_id']
 	join_room(match_details['room_id'])
+
+
+def validate_pawn(board,source,dest,player):
+
+	if player==0 and board[source[0]][source[1]]=='P':
+		return False
+	if player==1 and board[source[0]][source[1]]=='p':
+		return False
+
+	if board[source[0]][source[1]]=='p':
+		if dest[0]==source[0]+1 and board[source[0]+1][source[1]]=="":
+			return True
+		elif dest[0]==source[0]+2 and board[source[0]+1][source[1]]=="" and board[source[0]+2][source[1]]=="":
+			return True
+		elif dest[0]==source[0]+1 and dest[1]==source[1]-1 and board[source[0]+1][source[1]-1] in player2:
+			return True
+		elif dest[0]==source[0]+1 and dest[1]==source[1]+1 and board[source[0]+1][source[1]+1] in player2:
+			return True
+		else:	
+			return False
+	else:
+		if dest[0]==source[0]-1 and board[source[0]-1][source[1]]=="":
+			return True
+		elif dest[0]==source[0]-2 and board[source[0]-1][source[1]]=="" and board[source[0]-2][source[1]]=="":
+			return True
+		elif dest[0]==source[0]-1 and dest[1]==source[1]-1 and board[source[0]-1][source[1]-1] in player1:
+			return True
+		elif dest[0]==source[0]-1 and dest[1]==source[1]+1 and board[source[0]-1][source[1]+1] in player1:
+			return True
+		else:	
+			return False
+	return False
+
+
+
+def validate_king(board,source,dest,player):
+	if player==0:
+		enemies=player2
+	else: 
+		enemies=player1
+
+	if dest[0]==source[0]-1 and dest[1]==source[1]-1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]-1 and dest[1]==source[1] and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]-1 and dest[1]==source[1]+1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True 
+	elif dest[0]==source[0] and dest[1]==source[1]-1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0] and dest[1]==source[1]+1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]+1 and dest[1]==source[1]-1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]+1 and dest[1]==source[1] and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]+1 and dest[1]==source[1]+1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	else:
+		return False
+
+def validate_knight(board,source,dest,player):
+	if player==0:
+		enemies=player2
+	else: 
+		enemies=player1
+
+	if dest[0]==source[0]-2 and dest[1]==source[1]-1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]-2 and dest[1]==source[1]+1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]+2 and dest[1]==source[1]-1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True 
+	elif dest[0]==source[0]+2 and dest[1]==source[1]+1 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]-1 and dest[1]==source[1]+2 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]-1 and dest[1]==source[1]-2 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]+1 and dest[1]==source[1]+2 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	elif dest[0]==source[0]+1 and dest[1]==source[1]-2 and (board[dest[0]][dest[1]] in enemies or board[dest[0]][dest[1]]==""):
+		return True
+	else:
+		return False
+
+def validate_bishop(board,source,dest,player):
+	if player==0:
+		enemies=player2
+	else: 
+		enemies=player1
+
+	print("bishop")
+	print(player)
+	print(enemies)
+
+	diff=int(abs(dest[0]-source[0])/abs(dest[1]-source[1]))
+
+	if diff==1:
+		if dest[1]<source[1] and dest[0]<source[0]:
+			row_dir=-1
+			col_dir=-1
+		elif dest[1]<source[1] and dest[0]>source[0]:
+			row_dir=1
+			col_dir=-1
+		elif dest[1]>source[1] and dest[0]<source[0]:
+			row_dir=-1
+			col_dir=1
+		elif dest[1]>source[1] and dest[0]>source[0]:
+			row_dir=1
+			col_dir=1
+		start_row,start_col=source[0]+row_dir,source[1]+col_dir
+		while True:
+			if start_row==dest[0] and start_col==dest[1]:
+				if board[dest[0]][dest[1]]=="" or board[dest[0]][dest[1]] in enemies:
+					return True
+				else:
+					return False 
+			if board[start_row][start_col]!="":
+				return False
+			start_row=start_row+row_dir
+			start_col=start_col+col_dir
+	else:
+		return False
+	return False
+
+def validate_queen(board,source,dest,player):
+	if player==0:
+		enemies=player2
+	else: 
+		enemies=player1
+
+	if dest[1]==source[1] and dest[0]<source[0]:
+		row_dir=-1
+		col_dir=0
+	elif dest[1]==source[1] and dest[0]>source[0]:
+		row_dir=1
+		col_dir=0
+	elif dest[1]>source[1] and dest[0]==source[0]:
+		row_dir=0
+		col_dir=1
+	elif dest[1]<source[1] and dest[0]==source[0]:
+		row_dir=0
+		col_dir=-1
+	else :
+		diff=abs(dest[0]-source[0])/abs(dest[1]-source[1])
+		if diff==1:
+			if dest[1]<source[1] and dest[0]<source[0]:
+				row_dir=-1
+				col_dir=-1
+			elif dest[1]<source[1] and dest[0]>source[0]:
+				row_dir=1
+				col_dir=-1
+			elif dest[1]>source[1] and dest[0]<source[0]:
+				row_dir=-1
+				col_dir=1
+			elif dest[1]>source[1] and dest[0]>source[0]:
+				row_dir=1
+				col_dir=1
+		else:
+			return False
+
+	start_row,start_col=source[0]+row_dir,source[1]+col_dir
+
+	while True:
+		if start_row==dest[0] and start_col==dest[1]:
+			if board[dest[0]][dest[1]]=="" or board[dest[0]][dest[1]] in enemies:
+				return True
+			else:
+				return False 
+		if board[start_row][start_col]!="":
+			return False
+		start_row=start_row+row_dir
+		start_col=start_col+col_dir
+
+
+
+def validate_rook(board,source,dest,player):
+	if player==0:
+		enemies=player2
+	else: 
+		enemies=player1
+
+	if dest[1]==source[1] and dest[0]<source[0]:
+		row_dir=-1
+		col_dir=0
+	elif dest[1]==source[1] and dest[0]>source[0]:
+		row_dir=1
+		col_dir=0
+	elif dest[1]>source[1] and dest[0]==source[0]:
+		row_dir=0
+		col_dir=1
+	elif dest[1]<source[1] and dest[0]==source[0]:
+		row_dir=0
+		col_dir=-1
+	else:
+		return False
+
+	start_row,start_col=source[0]+row_dir,source[1]+col_dir
+
+	while True:
+		if start_row==dest[0] and start_col==dest[1]:
+			if board[dest[0]][dest[1]]=="" or board[dest[0]][dest[1]] in enemies:
+				return True
+			else:
+				return False 
+		if board[start_row][start_col]!="":
+			return False
+		start_row=start_row+row_dir
+		start_col=start_col+col_dir
+
+
+
+@socketio.on('move')
+def move(move_details):
+	match=ongoing_matches[session['room_id']]
+	source_row=int(move_details['r1'])-1
+	source_col=int(move_details['c1'])-1
+	dest_row=int(move_details['r2'])-1
+	dest_col=int(move_details['c2'])-1
+	print(f"turn of{match.turn}")
+
+	#Player makes a move even if it is not his turn
+	cond1=session['id']!=match.players[match.turn]
+	#Player selects a blank checkbox 
+	cond2=match.board[source_row][source_col]==""
+	#Player not making move on his own pieces
+	cond3=False
+	#source and destination same
+	cond4=dest_row==source_row and dest_col==source_col 
+
+	if match.turn==0:
+		if (match.board[source_row][source_col] not in ['p','n','r','k','q','b']):
+			cond3=True
+		if (match.board[dest_row][dest_col] in ['p','n','r','k','q','b']):
+			cond3=True
+
+	if match.turn==1:
+		if (match.board[source_row][source_col] not in ['P','N','R','K','Q','B']):
+			cond3=True
+		if (match.board[dest_row][dest_col] in ['P','N','R','K','Q','B']):
+			cond3=True
+
+	#print(match.board)
+
+	if  cond1 or cond2 or cond3 or cond4:
+		print("no valid move")
+		return socketio.emit('move_update',{'status':'Not a valid move'},room=session['id'])
+	else:
+		move_valid=False
+		if match.board[source_row][source_col] in ['p','P']:
+			move_valid=validate_pawn(match.board,[source_row,source_col],[dest_row,dest_col],match.turn)
+		elif match.board[source_row][source_col] in ['r','R']:
+			move_valid=validate_rook(match.board,[source_row,source_col],[dest_row,dest_col],match.turn)
+		elif match.board[source_row][source_col] in ['k','K']:
+			move_valid=validate_king(match.board,[source_row,source_col],[dest_row,dest_col],match.turn)
+		elif match.board[source_row][source_col] in ['n','N']:
+			move_valid=validate_knight(match.board,[source_row,source_col],[dest_row,dest_col],match.turn)
+		elif match.board[source_row][source_col] in ['q','Q']:
+			move_valid=validate_queen(match.board,[source_row,source_col],[dest_row,dest_col],match.turn)
+		elif match.board[source_row][source_col] in ['b','B']:
+			move_valid=validate_bishop(match.board,[source_row,source_col],[dest_row,dest_col],match.turn)
+
+		if move_valid==True:
+			data={}
+			data['status']='valid'
+			data['dest_row']=move_details['r2']
+			data['dest_col']=move_details['c2']
+			data['source_row']=move_details['r1']
+			data['source_col']=move_details['c1']
+			data['piece']=pieces[match.board[source_row][source_col]]
+			ongoing_matches[session['room_id']].board[dest_row][dest_col]=match.board[source_row][source_col]
+			ongoing_matches[session['room_id']].board[source_row][source_col]=""
+			if match.turn==0:
+				ongoing_matches[session['room_id']].turn=1
+			else:
+				ongoing_matches[session['room_id']].turn=0
+			return socketio.emit('move_update',data,room=session['room_id'])
+		else:
+			return socketio.emit('move_update',{'status':'Not a valid move'},room=session['id'])
+
 
 socketio.run(app,debug=True)
 
